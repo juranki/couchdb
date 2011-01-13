cea5807de7c1dbe92c482df2c0a99e7164c9a0d5
Juhani Rankimies <juhani.rankimies@ruukki.com>
2011-01-13 07:39:52 +0200

    Organize env_vars from bat to rebar.config

--- compile.bat
+++ compile.bat
@@ -1,10 +1,6 @@
 setlocal
 
-set CC=cl
-set DRV_CFLAGS=$ERL_CFLAGS
-set DRV_LDFLAGS=/LD /MD /link /LIBPATH:$ERL_EI_LIBS erl_interface.lib ei.lib
-
-set REBAR_CC_TEMPLATE=$CC /c $DRV_CFLAGS ~s /Fo~s
-set REBAR_LINK_TEMPLATE=$CC ~s /Fe~s $DRV_LDFLAGS
+set ICU_LIB=C:/relax/icu/lib
+set ICU_INCLUDE=C:/relax/icu/include
 
 rebar compile
