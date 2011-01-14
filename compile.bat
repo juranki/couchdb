97a3fcdee12529ff58fc1a7437ad8a9b7ec78303
unknown <Administrator@.juranki.com>
2011-01-14 20:53:38 +0000

    Couchdb starts but views don't work

--- compile.bat
+++ compile.bat
@@ -2,11 +2,24 @@ setlocal
 
 set ICU_LIB=C:/relax/icu/lib
 set ICU_INCLUDE=C:/relax/icu/include
-set JS_DIR=C:/relax/seamonkey-2.0.6/comm-1.9.1/mozilla/js/src
+set ICU_BIN=C:\relax\icu\bin
+::set JS_DIR=C:/relax/seamonkey-2.0.6/comm-1.9.1/mozilla/js/src
+set JS_DIR=C:/relax/comm-1.9.1/mozilla/js/src
 set CURL_LIB=C:/relax/curl-7.21.1/lib/LIB-Release
 set CURL_INCLUDE=C:/relax/curl-7.21.1/include
 
 call rebar compile
 
 copy couchjs\couchjs.exe .
-copy spawnkillable\spawnkillable.exe apps\couch\priv
+copy spawnkillable\couchspawnkillable.exe apps\couch\priv
+copy %ICU_BIN%\*.dll .
+
+pushd rel
+echo {data_dir, "../var/lib/couchdb"}.> dev.config
+echo {view_dir, "../var/lib/couchdb"}.>> dev.config
+echo {couchdb_port, 5984}.>> dev.config
+echo {ssl_port, 6985}.>> dev.config
+echo {prefix, ".."}.>> dev.config
+echo {view_dir, "../var/lib/couchdb"}.>> dev.config
+echo {node_name, "-sname dev"}.>> dev.config
+popd
---
+++ dev_rel.bat
@@ -0,0 +1,6 @@
+setlocal
+
+rd /s /q rel\dev
+call rebar generate target_dir=dev overlay_vars=dev.config
+
+copy *.dll rel\dev\bin
