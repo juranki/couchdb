85d5774d53f4dcf5126f5af50d65420f8fdbad2f
Juhani Rankimies <juhani.rankimies@ruukki.com>
2011-01-14 13:18:23 +0200

    compiles, but doesn't work yet

--- compile.bat
+++ compile.bat
@@ -2,8 +2,11 @@ setlocal
 
 set ICU_LIB=C:/relax/icu/lib
 set ICU_INCLUDE=C:/relax/icu/include
-set JS_DIR=C:/relax/comm-1.9.1/mozilla/js/src
+set JS_DIR=C:/relax/seamonkey-2.0.6/comm-1.9.1/mozilla/js/src
 set CURL_LIB=C:/relax/curl-7.21.1/lib/LIB-Release
 set CURL_INCLUDE=C:/relax/curl-7.21.1/include
 
-rebar compile
+call rebar compile
+
+copy couchjs\couchjs.exe .
+copy spawnkillable\spawnkillable.exe apps\couch\priv
